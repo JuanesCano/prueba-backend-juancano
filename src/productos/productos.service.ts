c203748650e455db2de74b9c41a09d5d5833b0e7
JuanesCano <juanestebancano.988@gmail.com>
2025-04-26 16:04:33 -0500

    Feat: ajuste de prisma schema

--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { UpdateProductoDto } from './dto/update-producto.dto';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 
 @Injectable()
@@ -9,17 +9,16 @@ export class ProductosService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(data: CreateProductoDto) {
-    const producto = await this.prisma.productos.create({  data: data as Prisma.ProductoCreateInput});
+    const producto = await this.prisma.productos.create({  data: data as Prisma.productosCreateInput});
     return producto;
   }
 
   async findAll() {
-    const productos = await this.prisma.productos.findMany();
-    return productos;
+    return this.prisma.productos.findMany();
   }
 
   async findOne(id: string) {
-    const producto = await this.prisma.productos.findFirst({ where: { id } });
+    const producto = await this.prisma.productos.findUnique({ where: { id } });
 
     if (!producto) {
       throw new NotFoundException(`Producto no encontrado con el id: ${id}`);
@@ -30,7 +29,7 @@ export class ProductosService {
 
   async update(id: string, data: UpdateProductoDto) {
     await this.findOne(id);
-    return this.prisma.productos.update({ where: { id },  data: data as Prisma.ProductoUpdateInput });
+    return this.prisma.productos.update({ where: { id },  data: data as Prisma.productosUpdateInput });
   }
 
   async remove(id: string) {
