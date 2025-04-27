626487175f36ade960e5ab19ed82a5019d17b5ef
JuanesCano <juanestebancano.988@gmail.com>
2025-04-27 13:36:57 -0500

    Feat: creacion de env template y health para verificar la coneccion en git actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductosModule } from './productos/productos.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CommonModule, ProductosModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), CommonModule, ProductosModule, PrismaModule, HealthModule],
   controllers: [],
   providers: [],
 })
