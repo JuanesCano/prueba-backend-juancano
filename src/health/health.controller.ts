8f8075d447b25fada73755fdfbe5b91b3d0f3c04
JuanesCano <juanestebancano.988@gmail.com>
2025-04-27 15:17:55 -0500

    configuracion de health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,15 +1,37 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
+import { 
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+  DiskHealthIndicator,
+  MemoryHealthIndicator
+} from '@nestjs/terminus';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Controller('health')
 export class HealthController {
-  constructor(private health: HealthCheckService) {}
+  constructor(
+    private health: HealthCheckService,
+    private prisma: PrismaService,
+    private db: TypeOrmHealthIndicator,
+    private disk: DiskHealthIndicator,
+    private memory: MemoryHealthIndicator
+  ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([
-      () => ({ status: 'OK', timestamp: new Date().toISOString() }),
+      () => this.prisma.$queryRaw`SELECT 1`
+        .then(() => ({ status: 'up' as const }))
+        .catch(() => ({ status: 'down' as const })),
+      
+      () => this.disk.checkStorage('storage', { 
+        path: '/', 
+        thresholdPercent: 0.9 
+      }),
+      
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
     ]);
   }
 }
